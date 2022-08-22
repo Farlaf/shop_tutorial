2760e04e2253c1350ae3ff84d102ed01634fa221
Farlaf <slepuhovoleg@gmail.com>
2022-08-22 12:34:58 +0400

    Add routing

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,8 +1,36 @@
 import React from "react";
-import { Swithc, Route, Redirect } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { authRoutes, publicRoutes } from "../routes";
+import { SHOP_ROUTE } from "../utils/consts";
 
 function DevicePage() {
-    return <div></div>;
+    const isAuth = true;
+
+    return (
+        <Routes>
+            {isAuth === true &&
+                authRoutes.map(({ path, Components }) => {
+                    return (
+                        <Route
+                            key={path}
+                            path={path}
+                            element={Components}
+                            exact
+                        />
+                    );
+                })}
+            {publicRoutes.map(({ path, Components }) => {
+                return (
+                    <Route key={path} path={path} element={Components} exact />
+                );
+            })}
+
+            <Route
+                path="*"
+                element={<Navigate to={SHOP_ROUTE} replace={true} />}
+            />
+        </Routes>
+    );
 }
 
 export default DevicePage;
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,4 +1,24 @@
+import {
+    ADMIN_ROUTE,
+    BASKET_ROUTE,
+    DEVICE_ROUTE,
+    LOGIN_ROUTE,
+    REGISTRATION_ROUTE,
+    SHOP_ROUTE,
+} from "./utils/consts";
 import Admin from "./pages/Admin";
+import Basket from "./pages/Basket";
+import Shop from "./pages/Shop";
+import Auth from "./pages/Auth";
+import DevicePage from "./pages/DevicePage";
 
-export const authRoutes = [{ patch: "/admin", Components: Admin }];
-export const publicRoutes = [];
+export const authRoutes = [
+    { path: ADMIN_ROUTE, Components: <Admin /> },
+    { path: BASKET_ROUTE, Components: <Basket /> },
+];
+export const publicRoutes = [
+    { path: SHOP_ROUTE, Components: <Shop /> },
+    { path: LOGIN_ROUTE, Components: <Auth /> },
+    { path: REGISTRATION_ROUTE, Components: <Auth /> },
+    { path: DEVICE_ROUTE + "/:id", Components: <DevicePage /> },
+];
